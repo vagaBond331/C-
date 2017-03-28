8eefb1f3159f231bb441b36afd8cabe82f238336
Rai <Rai@DESKTOP-5KDD5S3>
2017-03-28 21:15:07 +0700

    Add project files.

---
+++ SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OlympiaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Admin()
+        {
+            ViewBag.NumPlayer = 0;
+            ViewBag.UserID = Session["logUserID"];
+            ViewBag.UserName = Session["logUserName"];
+            return View();
+        }
+
+        public ActionResult Player()
+        {
+            ViewBag.UserID = Session["logUserID"];
+            ViewBag.UserName = Session["logUserName"];
+            return View();
+        }
+    }
+}
---
+++ SignalRChat/Controllers/Round1Controller.cs
@@ -0,0 +1,55 @@
+﻿using OlympiaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OlympiaApp.Controllers
+{
+    public class Round1Controller : Controller
+    {
+        private OlympiaEntities db = new OlympiaEntities();
+        private List<Vong1> list = new List<Vong1>();
+
+        // GET: Round1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            string[] player = this.Request.QueryString["joined"].ToString().Split(':');
+            int num = int.Parse(player[0]);
+            ViewBag.num = num;
+            if (num >= 1) ViewBag.Player1 = player[1];
+            if (num >= 2) ViewBag.Player2 = player[2];
+            if (num >= 3) ViewBag.Player3 = player[3];
+            if (num >= 4) ViewBag.Player4 = player[4];
+            getQues1(num);
+            ViewData["listQues"] = list;
+
+            return View();
+        }
+
+        public ActionResult Player()
+        {
+            string playerID = this.Request.QueryString["id"];
+            string playerName = db.HocSinh.Find(playerID).Ten;
+            ViewBag.PlayerID = playerID;
+            ViewBag.PlayerName = playerName;
+            ViewData["listQues"] = list;
+            return View();
+        }
+
+        public void getQues1(int num)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 3 * num; i++)
+            {
+                list.Add(db.Vong1.Find(r.Next(1, db.Vong1.ToList().Count())));
+            }
+        }
+    }
+}
